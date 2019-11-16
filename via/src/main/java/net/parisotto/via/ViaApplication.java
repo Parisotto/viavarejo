73cd23d5ce1a48d978a30a3b42013060b67733ef
Parisotto <parisotto@gmail.com>
2019-11-16 15:21:01 -0300

    Checagem de atualizações

--- via/src/main/java/net/parisotto/via/ViaApplication.java
+++ via/src/main/java/net/parisotto/via/ViaApplication.java
@@ -1,5 +1,6 @@
 package net.parisotto.via;
 // Iniciado em 16/11/2019 13:15 ELP
+// Teste de back-end Via Varejo
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
