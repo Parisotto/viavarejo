2eee8a0f64a43ed7f392bc8b0d29bf7132c48b0a
Edson L Parisotto <parisotto@gmail.com>
2019-11-16 15:13:21 -0300

    Update de verificação

--- via/src/main/java/net/parisotto/via/ViaApplication.java
+++ via/src/main/java/net/parisotto/via/ViaApplication.java
@@ -1,5 +1,5 @@
 package net.parisotto.via;
-
+// Iniciado em 16/11/2019 13:15 ELP
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
