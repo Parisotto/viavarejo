c6f14bff41ab0bc3fc1d8549585bd3728f44ac3f
Parisotto <parisotto@gmail.com>
2019-11-17 17:32:51 -0300

    Finalizações do sistema

--- via/src/main/java/net/parisotto/via/controllers/ProdutoController.java
+++ via/src/main/java/net/parisotto/via/controllers/ProdutoController.java
@@ -2,11 +2,15 @@ package net.parisotto.via.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.parisotto.via.models.Parcela;
 import net.parisotto.via.models.Produto;
 import net.parisotto.via.repository.ProdutoRepository;
 
@@ -14,6 +18,7 @@ import net.parisotto.via.repository.ProdutoRepository;
 public class ProdutoController {
 	@Autowired
 	private ProdutoRepository pr;
+	double valorProduto = 0;
 	
 	@RequestMapping(value="/cadastrarProduto", method=RequestMethod.GET)
 	public String form() {
@@ -35,10 +40,26 @@ public class ProdutoController {
 	}
 	
 	@RequestMapping("/{idprod}")
-	public ModelAndView comprarProduto(@PathVariable("idprod") long idprod) {
+	public ModelAndView comprarProduto(Model model, @PathVariable("idprod") long idprod) {
 		Produto produto = pr.findByIdprod(idprod);
-		ModelAndView mv = new ModelAndView("comprarProduto");
+		valorProduto = Double.parseDouble(produto.getValor());
+		ModelAndView mv = new ModelAndView("produto/comprarProduto");
 		mv.addObject("produto", produto);
+		model.addAttribute("parcela", new Parcela());
 		return mv;
+	}
+	
+	@PostMapping("/parcelamento")
+	public String calcular(Model model,@ModelAttribute Parcela parcelas) {
+		double principal = valorProduto - parcelas.getEntrada();
+		int meses = parcelas.getNumeroParcelas();
+		double taxa = 0.0115; // 1,15% a.m.
+		
+		double parcelado = (principal * Math.pow(1 + taxa, meses)) / meses;
+		String strParcela = String.format("%,.2f", parcelado);
+		model.addAttribute("parcelado", strParcela);
+		model.addAttribute("meses", meses);
+		model.addAttribute("taxa", taxa * 100);
+		return "produto/listaparcelas";
 	}
 }
---
+++ via/src/main/java/net/parisotto/via/models/Parcela.java
@@ -0,0 +1,20 @@
+package net.parisotto.via.models;
+
+public class Parcela {
+	
+	private int numeroParcelas;
+	private double entrada;
+	
+	public int getNumeroParcelas() {
+		return numeroParcelas;
+	}
+	public void setNumeroParcelas(int numeroParcelas) {
+		this.numeroParcelas = numeroParcelas;
+	}
+	public double getEntrada() {
+		return entrada;
+	}
+	public void setEntrada(double entrada) {
+		this.entrada = entrada;
+	}
+}
